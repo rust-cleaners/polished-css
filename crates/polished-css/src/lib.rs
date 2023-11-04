91e932acf652e23dade06e71a25a524a01f9d3a3
xeho91 <xeho91@pm.me>
2023-11-04 23:22:00 +0800

    trigger format issue

--- crates/polished-css/src/lib.rs
+++ crates/polished-css/src/lib.rs
@@ -28,15 +28,15 @@
 #![allow(clippy::module_name_repetitions)]
 #![allow(ambiguous_glob_reexports)]
 
-#[cfg(feature = "atomic")]
-pub mod atomic;
-pub mod data_type;
-pub mod function;
-mod macros;
-pub mod prelude;
-pub mod property;
-pub mod selector;
-pub mod style;
-#[cfg(test)]
-mod test;
-pub mod utils;
+    #[cfg(feature = "atomic")]
+    pub mod atomic;
+    pub mod data_type;
+    pub mod function;
+    mod macros;
+    pub mod prelude;
+    pub mod property;
+    pub mod selector;
+    pub mod style;
+    #[cfg(test)]
+    mod test;
+    pub mod utils;

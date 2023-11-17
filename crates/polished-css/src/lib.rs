719ce05fab94b34f50a3271d22552cb2cb005dee
Mateusz Kadlubowski <xeho91@pm.me>
2023-11-17 21:50:27 +0800

    chore(Cargo): Unify `lints` at the workspace manifest (#51)

--- crates/polished-css/src/lib.rs
+++ crates/polished-css/src/lib.rs
@@ -1,31 +1,4 @@
 #![feature(const_trait_impl)]
-#![deny(
-	clippy::all,
-    // Groups
-	clippy::cargo,
-	clippy::complexity,
-	clippy::correctness,
-	clippy::nursery,
-	clippy::pedantic,
-	clippy::perf,
-	clippy::suspicious,
-    // Adjustments
-	clippy::cast_possible_truncation,
-	clippy::cast_precision_loss,
-	clippy::unwrap_used,
-	// missing_docs,
-	missing_debug_implementations,
-	rust_2018_idioms,
-	unused_must_use,
-	unused_lifetimes,
-	unused_results
-)]
-#![warn(unused_tuple_struct_fields, unused_crate_dependencies)]
-#![allow(
-	clippy::derive_partial_eq_without_eq,
-	clippy::module_name_repetitions,
-	ambiguous_glob_reexports
-)]
 
 #[cfg(feature = "atomic")]
 pub mod atomic;
